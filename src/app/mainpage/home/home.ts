34c80bd73f92f68fd5b36e15e74caa80fef95ad9
manogna9866 <manogna.vita@gmail.com>
2026-04-08 15:13:36 +0530

    redesigned pages,modified by manogna

--- src/app/mainpage/home/home.ts
+++ src/app/mainpage/home/home.ts
@@ -9,11 +9,12 @@ import { Footer } from '../footer/footer';
 import { Section5 } from './section5/section5';
 import { Section6 } from './section6/section6';
 import { Section7 } from './section7/section7';
+import { Corousel } from "./corousel/corousel";
 
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink,Header,Section1,Section2,Section3,Section4,Footer,Section5,Section6,Section7],
+  imports: [RouterLink, Header, Section1, Section2, Section3, Section4, Footer, Section5, Section6, Section7, Corousel],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
